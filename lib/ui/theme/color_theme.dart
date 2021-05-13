dc309285573a11ecb890467c1b85850cd6fdc09d
Alejandro Santiago <44524995+alestiago@users.noreply.github.com>
2021-05-13 18:17:37 +0100

    Started initial setup

---
+++ lib/ui/theme/color_theme.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class ColorTheme {
+  static const primary = Color(0xff123123);
+}
---
+++ lib/ui/theme/text_theme.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class TextTheme {
+  static final headline1 = TextStyle();
+}
