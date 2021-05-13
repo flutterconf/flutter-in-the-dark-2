43e946cde63e77e6e9eb4619baa72634a9342b8c
fgadea <fgadea95@outlook.com>
2021-05-13 19:22:05 +0200

    null safety

--- lib/data/character.dart
+++ lib/data/character.dart
@@ -1,7 +1,7 @@
 abstract class Character {
-  final int flightLevel;
-  final int flameLevel;
-  final int readMindsLevel;
+  final int? flightLevel;
+  final int? flameLevel;
+  final int? readMindsLevel;
 
   Character({this.flightLevel, this.flameLevel, this.readMindsLevel});
 }
